f1e937bd9fca917c0e477ff604c08f67fc756ec5
U-rogue\buble <buble@rogue.(none)>
2014-04-03 19:44:51 -0700

    scratch directory

---
+++ scratch/README.txt
@@ -0,0 +1,5 @@
+Scratch directory :
+-------------------
+
+. old multiclass files
+. tryouts
--- scripts/generateplayerfiles.py
+++ scripts/generateplayerfiles.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-######### ./outputplayer elf fighter Elf Fighter
+######### ./generateplayerfiles.py elf fighter Elf Fighter ELF FIGHTER
 
 # Copyright (C) Johan Ceuppens 2014
 # This program is free software: you can redistribute it and/or modify
@@ -23,14 +23,16 @@ import sys
 # command line args in a list
 args = str(sys.argv)
 
-if len(sys.argv) < 5 or len(sys.argv) > 5:
-	print "Usage : outputplayer.py [playerrace] [playerclass] [Capitalplayerrace] [Capitalplayerclass]\n"	
+if len(sys.argv) < 7 or len(sys.argv) > 7:
+	print "Usage : outputplayer.py [playerrace] [playerclass] [Capitalplayerrace] [Capitalplayerclass] [RACE] [CLASS]\n"	
 	sys.exit
 
 race = sys.argv[1]
 klass = sys.argv[2]
 Race = sys.argv[3]
 Klass = sys.argv[4]
+RACE = sys.argv[5]
+KLASS = sys.argv[6]
 
 
 def outputplayerracefile():
@@ -106,5 +108,35 @@ def outputplayerraceresourcesfile():
 	
 	f.write("\n")
 
+def outputplayerraceclassfile():
+	f = open("./player" + race + klass + ".py", "w")    ### .close() # touch file
+	if f == None:
+		print "Error : opening file\n"
+		sys.exit
+
+	for line in gplheader2list():
+		f.write(line)
+
+	f.write("\n")
+
+	f.write("import pygame\n")
+	f.write("from pygame.locals import *\n")
+	f.write("from playerbase import *\n")
+	f.write("from player" + race + "resources" + " import *\n")
+	f.write("\n")
+	f.write("class Player" + Race + Klass + "(PlayerBase, Player" + Race + "Resources):\n")
+	f.write("\t\"Player " + Race + " " + Klass + "\"\n")
+	f.write("\tdef __init__(self):\n")
+	f.write("\t\tPlayerBase.__init__(self,PlayerBase." + RACE + ", PlayerBase." + KLASS + ")\n")
+	f.write("\t\tPlayer" + Race + "Resources.__init__(self)\n")
+	f.write("\n")
+	f.write("\tdef askclass(self):\n")
+	f.write("\t\treturn " + "\"" + Klass + "\"\n") #### FIXME Magic User won't work
+	f.write("\n")
+	f.write("\tdef askrace(self):\n")
+	f.write("\t\treturn " + "\"" + Race + "\"\n")
+	f.write("\n")
+     	
 outputplayerracefile() 
 outputplayerraceresourcesfile() 
+outputplayerraceclassfile() 
